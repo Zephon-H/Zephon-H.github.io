14dfbb0aec5db5bf770f3db7279062da32578837
Zephon <37538679+Zephon-H@users.noreply.github.com>
2022-02-28 18:28:32 +0800

    Update imgData.js

--- js/imgData.js
+++ js/imgData.js
@@ -1,4 +1,4 @@
-var imgUrl = "https://www.guanyujiang.com/api/bingapi.php?rand=true";
+var imgUrl = "https://api.dujin.org/bing/1920.php";
 
 document.write('<style>body{background-color: #1685a9; width:100%; height:100%; background:url(' + imgUrl + '); background-attachment:fixed; -moz-background-size: cover; -ms-background-size: cover; -webkit-background-size: cover; background-size: cover; background-position: center; z-index: 1;}</style>');
 

a0c8a02c5aed4d0253706c5748be01800ed7d9c5
Zephon-H <1528498238@qq.com>
2022-02-28 18:23:04 +0800

    制作个人导航页

---
+++ js/imgData.js
@@ -0,0 +1,8 @@
+var imgUrl = "https://www.guanyujiang.com/api/bingapi.php?rand=true";
+
+document.write('<style>body{background-color: #1685a9; width:100%; height:100%; background:url(' + imgUrl + '); background-attachment:fixed; -moz-background-size: cover; -ms-background-size: cover; -webkit-background-size: cover; background-size: cover; background-position: center; z-index: 1;}</style>');
+
+// document.write('<style>body{background-color: #1685a9; width:100%; height:100%; background:url(' + imgUrl + '); background-attachment:fixed; -moz-background-size: cover; -ms-background-size: cover; -webkit-background-size: cover; background-size: cover; background-position: center; z-index: 1;}</style>');
+
+
+
